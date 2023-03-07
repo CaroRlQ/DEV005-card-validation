f01120d282f5484ebaea39bf699df1d9a28f2421
unknown <caroraquelq@gmail.com>
2023-03-07 12:51:27 -0500

    Acrualización

--- src/index.js
+++ src/index.js
@@ -1,3 +1,104 @@
 import validator from './validator.js';
 
-console.log(validator);
+//LLAMAR EVENTO VALIDACION DE LLENADO DE INPUT--
+
+document.getElementById('sendForm').addEventListener('click', sendForm)
+
+//FUNCION PARA SE EFECTUE PRIMERO LA FUNCION DE VALIDACION DE LLENADO DE INPUT--
+function sendForm(){
+  const inputValid=validInput();
+  
+  if(inputValid===true){
+    validLuhn();
+  }
+  
+}
+
+//FUNCION DE VALIDACION DE LLENADO DE INPUT--
+function validInput() {
+  
+  const name = document.getElementById('name').value;
+  const number = document.getElementById('inputNumberInsert').value;
+  if (name === '') {
+    document.getElementById('errorName').style.display = 'block';
+    document.getElementById('name').focus();
+  }
+  else if (number === '') {
+    document.getElementById('errorNumber').style.display = 'block';
+    document.getElementById('inputNumberInsert').focus();
+  } 
+  else{
+    return true
+  }
+  
+}
+
+
+//FUNCION PARA VALIDACION DEL ALGORITMO DE LUHN--
+function validLuhn() {
+  
+  const number = document.getElementById('inputNumberInsert').value;
+  const validLuhn = validator.isValid(number);
+  console.log(validLuhn);
+
+  if (validLuhn === true) {
+    document.getElementById('infValid').style.display = 'block';
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('noValid').style.display = 'none';
+  }
+  else {
+    document.getElementById('noValid').style.display = 'block';
+    document.getElementById('infValid').style.display = 'none';
+    document.getElementById('form').style.display = 'none';
+  }
+}
+
+//LLAMAR EVENTO AL BOTON "REGRESAR"--
+document.getElementById('comeBack').addEventListener('click', back);
+//FUNCION PARA FUNCION DE BOTON "REGRESAR"--
+function back() {
+  document.getElementById('form').style.display = 'block';
+  document.getElementById('infValid').style.display = 'none';
+  document.getElementById('noValid').style.display = 'none';
+}
+
+document.getElementById('sendForm').addEventListener('click', inputMaskNumber);
+//FUNCION PARA ENMASCARAR DIGITOS--
+function inputMaskNumber() {
+  const name = document.getElementById('name').value;
+  const number = document.getElementById('inputNumberInsert').value;
+  const inputMask = number;
+  const numberMask = validator.maskify(inputMask);
+  const answer = document.getElementById('answer');
+  const answer2 = document.getElementById('answer2');
+  answer.textContent = `Hola ${name}, el número de tarjeta ${numberMask}` ;
+  answer2.textContent = `Hola ${name}, el número de tarjeta ${numberMask};` ;
+}
+
+//EVENTO PARA BORRAR LLENADO INPUT AL HACER CLICK EN "REGRESAR"--
+document.getElementById('comeBack').addEventListener('click', clean);
+
+
+//EVENTO PARA  LIMPIAR INPUTS Y REGRESAR A LA PANTALLA INICIAL--
+document.getElementById('again').addEventListener('click', eraser);
+
+//FUNCION PARA LIMPIAR INPUTS E "INTENTAR OTRA VEZ" --
+function eraser(){
+  again();
+  clean();
+}
+
+//FUNCION PARA REGRESAR A LA PANTALLA INICIAL AL HACER CLICK EN "INTENTAR OTRA VEZ"--
+function again() {
+  document.getElementById('noValid').style.display = 'none';
+  document.getElementById('infValid').style.display = 'none';
+  document.getElementById('form').style.display = 'block';
+}
+//FUNCION PARA LIMPIAR INPUTS --
+function clean() {
+  document.getElementById('name').value = '';
+  document.getElementById('errorName').style.display = 'none';
+  document.getElementById('inputNumberInsert').value = '';
+  document.getElementById('errorNumber').style.display = 'none';
+  document.getElementById('name').focus();
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,87 @@
 const validator = {
-  // ...
-};
+
+  isValid: function (number) {
+    
+    // CONVERSION DE STRING A ARRAY Y NUMBER--
+    const inputNumber = number.split('').map((number) => { return parseInt(number) });
+
+    //INVERTIR ARRAY -- 
+
+    const arrayInvert=inputNumber.reverse();
+
+    console.log(inputNumber);
+
+    //NUEVO ARRAY CON NUMEROS IMPARES "NUM1"--
+    const num1 = [];
+
+    for (let i = 1; i < arrayInvert.length; i = i + 2) {
+      num1.push(arrayInvert[i]);
+    }
+
+    for (let i = 0; i < num1.length; i++) { // MULTIPLICACION X2 DE NUMEROS EN POSICION IMPAR--
+      num1[i] = num1[i] * 2;
+      if (num1[i] > 9) {
+        num1[i] = num1[i] - 9;
+      }
+    }
+    console.log('num1', num1);
+
+    //SUMA DE DIGITOS DE ARRAY NUM1--
+    let sum1 = 0;
+    for (let i = 0; i < num1.length; i++) {
+      sum1 += num1[i];
+
+    }
+
+    console.log('sum1', sum1)
+
+
+
+    //ARRAY CON NUMEROS PARES "NUM2"
+    const num2 = [];
+    for (let i = 0; i < arrayInvert.length; i = i + 2) {
+      num2.push(arrayInvert[i]);
+    }
+    console.log('num2', num2);
+
+    ///SUMA DE DIGITOS DE ARRAY NUM2--
+    let sum2 = 0;
+    for (let i = 0; i < num2.length; i++) {
+      sum2 += num2[i];
+    }
+    console.log('sum2', sum2);
+
+    //SUMA DE ARRAY NUM1 +NUM2
+   
+    const sumTotal = sum1 + sum2;
+    console.log('sumTotal',sumTotal);
+    
+    //VERIFICACION SI SUMATOTAL ES MULTIPLO DE 10--
+
+    if (sumTotal % 10 === 0){
+      return true
+    }else { return false
+    }
+    
+  },
+
+
+
+  maskify: function (inputMask) {
+
+    //RECORRER CUATRO POSICIONES DE IZQ=>DERECHA PARA OMITIR ENMASCARAR LOS ULTIMOS DIGITOS
+
+    const inMask = inputMask.split('');
+    const mask = inMask.length - 4;
+
+    //ENMASCARAR LOS DIGITOS GUARDADOS EN EL SLICE MASK
+    for (let i = 0; i < mask; i++) {
+      inMask[i] = '#';
+    }
+    return inMask.join('');
+
+    //MOSTRAR LOS OBJETOS SIN COMILLA
+  }
+}
 
 export default validator;
